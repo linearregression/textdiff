92207f97ea20a741e6ee797b18242942ca94bb3f
Max Kolyanov <m.kolyanov@gmail.com>
2015-07-15 16:35:13 +0300

    Removed verbump

--- verbump.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs'),
-	a = JSON.parse(fs.readFileSync('package.json')),
-	b = a.version.split('.')
-
-b[2] = process.argv[2]
-a.version = b.join('.')
-fs.writeFileSync('package.json', JSON.stringify(a, null, "  "))
